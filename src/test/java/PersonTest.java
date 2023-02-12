3a6ca2d333f7319212d69d213c790f344e629c03
MiguelMC <mcmiguel313@gmail.com>
2023-02-12 21:26:22 +0100

    creo qu finalmente esta bien todo

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -32,6 +32,16 @@ public class PersonTest {
         m3=null;
     }
 
+    /**
+     * CASOS DE PRUEBA:
+     * 1. Dar error si la lista esta vacia
+     * 2. Comprobar que si la lista solo tiene un hombre muestra su edad (media de 1 es su valor)
+     * 3. Comprobar que si la lista solo tiene una mujer muestra su edad (media de 1 es su valor)
+     * 4. Comprobar que si la lista tiene 1 hombre y 1 mujer salen sus edades
+     * 5. Si detecta un genero que no es "male" o "female" dar error
+     * 6. Hacer la prueba con 3 hombres y 3 mujeres
+     */
+
     @Test
     void listaVaciaError(){
         assertThrows(RuntimeException.class, ()->p.averageAgePerGender(lista));
